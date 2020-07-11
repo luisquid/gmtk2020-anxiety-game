c847003277ba2d7ed1edd360f9a4a5de8f249c3d
Jorge Luis De Alba <jorlu934@gmail.com>
2020-07-10 22:58:35 -0500

    Gameplay prototype, imported puppetmaster plugin for active ragdolls

---
+++ Assets/_gmtk2020/Scripts/AnxietyKey.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnxietyKey : MonoBehaviour
+{
+    public CanvasGroup keyGroup;
+    public Text keyText;
+
+    public KeyCode kcode;
+    public float duration;
+
+    private float timePressed;
+
+    private void Start()
+    {
+        timePressed = duration;
+    }
+
+    public bool IsCompleted()
+    {
+        if(Input.GetKey(kcode))
+        {
+            timePressed -= Time.deltaTime;
+
+            keyGroup.alpha = timePressed / duration;
+
+            if(keyGroup.alpha <= 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Assets/_gmtk2020/Scripts/KeyController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    public int maxKeys = 5;
+    public Transform spawnArea;
+    public AnxietyKey keyPrefab;
+
+    public List<AnxietyKey> spawnedKeys = new List<AnxietyKey>();
+    private List<Transform> availableSpaces = new List<Transform>();
+    private List<KeyCode> allowedKeys = new List<KeyCode>() { KeyCode.Q, KeyCode.W, KeyCode.E, KeyCode.A, KeyCode.S, KeyCode.D, KeyCode.Z, KeyCode.X, KeyCode.C,KeyCode.B, KeyCode.N,KeyCode.M,KeyCode.J, KeyCode.K, KeyCode.L,KeyCode.I,KeyCode.O,KeyCode.P };
+
+    private void Start()
+    {
+        for (int i = 0; i < spawnArea.childCount; i++)
+        {
+            availableSpaces.Add(spawnArea.GetChild(i));
+        }
+
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < spawnedKeys.Count; i++)
+        {
+            if(spawnedKeys[i].IsCompleted())
+            {
+                DespawnKey(spawnedKeys[i]);
+            }
+        }
+    }
+
+    void SpawnKey()
+    {
+        if(spawnedKeys.Count < maxKeys)
+        {
+            var k = Instantiate(keyPrefab,availableSpaces[Random.Range(0,availableSpaces.Count)]);
+            availableSpaces.Remove(k.transform.parent);
+
+            //Randomize keycode
+            k.kcode = allowedKeys[Random.Range(0, allowedKeys.Count)];
+            k.keyText.text = k.kcode.ToString();
+            allowedKeys.Remove(k.kcode);
+
+            spawnedKeys.Add(k);
+        }
+    }
+
+    void DespawnKey(AnxietyKey k)
+    {
+        allowedKeys.Add(k.kcode);
+        availableSpaces.Add(k.transform.parent);
+        spawnedKeys.Remove(k);
+
+        Destroy(k.gameObject);
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while(true)
+        {
+            SpawnKey();
+
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
